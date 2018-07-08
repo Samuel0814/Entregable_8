e1d9b0e2e1b2ad10b4001278cec1b080d4a21e30
Samuel Alexander Reyes Diaz <samuel_reyes1@ucne.edu.do>
2018-07-08 18:33:46 -0400

    Entidad Libro

--- RegistroLibros/Entidades/Libros.cs
+++ RegistroLibros/Entidades/Libros.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 
@@ -7,6 +8,26 @@ namespace RegistroLibros.Entidades
 {
     public class Libros
     {
+        [Key]
 
+        public int LibroId { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public string Siglas { get; set; }
+
+        public string Tipo { get; set; }
+
+
+        public Libros()
+        {
+            LibroId = 0;
+            Fecha = DateTime.Now;
+            Descripcion = string.Empty;
+            Siglas = string.Empty;
+            Tipo = string.Empty;
+        }
     }
 }
