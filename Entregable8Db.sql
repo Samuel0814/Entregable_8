ca36eab9b58e03ba077e1e4270282aa0d010eabc
Samuel Alexander Reyes Diaz <samuel_reyes1@ucne.edu.do>
2018-07-08 18:17:41 -0400

    Creacion de las carpetas, clases, base de datos, contexto, formularios de registro y consulta, ademas del cambio de nombre de FormularioPrincipal

---
+++ Entregable8Db.sql
@@ -0,0 +1,4 @@
+Create database Entregable8Db
+go
+use Entregable8Db
+go
---
+++ RegistroLibros/DAL/Contexto.cs
@@ -0,0 +1,23 @@
+﻿using RegistroLibros.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace RegistroLibros.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Libros> Libros { get; set; }
+
+        public Contexto() : base("ConStr")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
